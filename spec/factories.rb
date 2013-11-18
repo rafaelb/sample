4b7939e6fef9cc3026f876f696eb94f1125fe841
Rafael Borochov <rafael.borochov@gmail.com>
2013-11-18 15:53:08 +0200

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "Rafael"
+    email                 "rafael@b.com"
+    password              "abcdef"
+    password_confirmation "abcdef"
+  end
+end
